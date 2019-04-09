8bd8346efb232b749321f72006e5bf2992449444
khalednabulsi <nabulsi.40@googlemail.com>
2019-04-09 16:04:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
 client.on('guildMemberAdd', member => {
     let memberTag = member.user.id;
     member.guild.channels.get('476526110046420994').send("Welcome to Rs Wanderers! "+"<@" + member.id +"> Enjoy your stay and don't forget to read the rules first by clicking here, <#476707469549371392>. Afterwards maybe <#476863150420131840> so we can all get to know you!"); 
-    client.guilds.get('564375603952877568').channels.get('564381698825912329').send(member.id);
+    client.guilds.get('564375603952877568').channels.get('564381698825912329').send(member.user.username);
 });
 client.on('guildMemberAdd', (guildMember) => {
     guildMember.addRole(guildMember.guild.roles.find(role => role.name === "Smiley"));
